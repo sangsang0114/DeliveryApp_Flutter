89ca8d29367795c2db41625d3b78b6b300a2c91a
yoon <envy15@skuniv.ac.kr>
2024-01-18 23:16:21 +0900

    플러터 실전 앱 개발 준비

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: _App(),
+    ),
+  );
+}
+
+class _App extends StatelessWidget {
+  const _App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Container(),
+      ),
+    );
+  }
+}
