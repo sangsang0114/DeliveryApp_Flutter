91347fa931cdc1784f75598b2d60f415db55e57b
yoon <envy15@skuniv.ac.kr>
2024-07-04 18:08:59 +0900

    feat : 리포지터리 생성

---
+++ lib/restaurant/repository/restaurant_repository.dart
@@ -0,0 +1,19 @@
+import 'package:actual/restaurant/model/restaurant_detail_model.dart';
+import 'package:dio/dio.dart';
+import 'package:retrofit/http.dart';
+
+part 'restaurant_repository.g.dart';
+
+@RestApi()
+abstract class RestaurantRepository {
+  factory RestaurantRepository(Dio dio, {String baseUrl}) =
+      _RestaurantRepository;
+
+  // @GET('/')
+  // paginate();
+
+  @GET('/{id}')
+  Future<RestaurantDetailModel> getRestaurantDetail({
+    @Path() required String id,
+  });
+}
--- lib/restaurant/view/restaurant_detail_screen.dart
+++ lib/restaurant/view/restaurant_detail_screen.dart
@@ -2,6 +2,7 @@ import 'package:actual/common/layout/default_layout.dart';
 import 'package:actual/product/component/product_card.dart';
 import 'package:actual/restaurant/component/restaurant_card.dart';
 import 'package:actual/restaurant/model/restaurant_detail_model.dart';
+import 'package:actual/restaurant/repository/restaurant_repository.dart';
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 
@@ -15,41 +16,36 @@ class RestaurantDetailScreen extends StatelessWidget {
     required this.id,
   }) : super(key: key);
 
-  Future<Map<String, dynamic>> getRestaurantDetail() async {
+  Future<RestaurantDetailModel> getRestaurantDetail() async {
     final dio = Dio();
-    final accessToken = await storage.read(key: ACCESS_TOKEN_KEY);
-    final resp = await dio.get(
-      'http://$ip/restaurant/$id',
-      options: Options(
-        headers: {
-          'authorization': 'Bearer $accessToken',
-        },
-      ),
-    );
-    return resp.data;
+    final repository =
+        RestaurantRepository(dio, baseUrl: 'http://$ip/restaurant');
+    return repository.getRestaurantDetail(id: id);
   }
 
   @override
   Widget build(BuildContext context) {
     return DefaultLayout(
       title: '불타는 떡볶이',
-      child: FutureBuilder<Map<String, dynamic>>(
+      child: FutureBuilder<RestaurantDetailModel>(
         future: getRestaurantDetail(),
-        builder: (_, AsyncSnapshot<Map<String, dynamic>> snapshot) {
+        builder: (_, AsyncSnapshot<RestaurantDetailModel> snapshot) {
+          if(snapshot.hasError){
+            return Center(child: Text(snapshot.error.toString()),);
+          }
           if (!snapshot.hasData) {
             return const Center(
               child: CircularProgressIndicator(),
             );
           }
 
-          final item = RestaurantDetailModel.fromJson(snapshot.data!);
           return CustomScrollView(
             slivers: [
               renderTop(
-                model: item,
+                model: snapshot.data!,
               ),
               renderLabel(),
-              renderProducts(products: item.products),
+              renderProducts(products: snapshot.data!.products),
             ],
           );
         },
