7c8aadc8d15e76d26a01f4cf48fc693c91b26b48
yoon <envy15@skuniv.ac.kr>
2024-07-04 16:17:35 +0900

    feat : sliverList 구현

--- lib/restaurant/view/restaurant_detail_screen.dart
+++ lib/restaurant/view/restaurant_detail_screen.dart
@@ -10,24 +10,60 @@ class RestaurantDetailScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return DefaultLayout(
       title: '불타는 떡볶이',
-      child: Column(
-        children: [
-          RestaurantCard(
-            image: Image.asset('asset/img/food/ddeok_bok_gi.jpg'),
-            name: '불타는 떡볶이',
-            tags: ['떡볶이', '맛있음', '치즈'],
-            ratingsCount: 5,
-            deliveryTime: 20,
-            deliveryFee: 100,
-            ratings: 4.5,
-            isDetail: true,
-            detail: 'asdf',
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 16.0),
-            child: ProductCard(),
-          ),
+      child: CustomScrollView(
+        slivers: [
+          renderTop(),
+          renderLabel(),
+          renderProducts(),
         ],
+      ),
+    );
+  }
+
+  SliverToBoxAdapter renderTop() {
+    return SliverToBoxAdapter(
+      child: RestaurantCard(
+        image: Image.asset('asset/img/food/ddeok_bok_gi.jpg'),
+        name: '불타는 떡볶이',
+        tags: ['떡볶이', '맛있음', '치즈'],
+        ratingsCount: 5,
+        deliveryTime: 20,
+        deliveryFee: 100,
+        ratings: 4.5,
+        isDetail: true,
+        detail: 'asdf',
+      ),
+    );
+  }
+
+  renderProducts() {
+    return SliverPadding(
+      padding: EdgeInsets.symmetric(horizontal: 16.0),
+      sliver: SliverList(
+        delegate: SliverChildBuilderDelegate(
+          (context, index) {
+            return const Padding(
+              padding: EdgeInsets.only(top: 16.0),
+              child: ProductCard(),
+            );
+          },
+          childCount: 10,
+        ),
+      ),
+    );
+  }
+
+  SliverPadding renderLabel() {
+    return const SliverPadding(
+      padding: EdgeInsets.symmetric(horizontal: 16.0),
+      sliver: SliverToBoxAdapter(
+        child: Text(
+          '메뉴',
+          style: TextStyle(
+            fontSize: 18.0,
+            fontWeight: FontWeight.w500,
+          ),
+        ),
       ),
     );
   }
