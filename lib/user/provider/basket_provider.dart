92e33d211b32cd6891f929f75f92c8c66aa8d9f5
yoon <envy15@skuniv.ac.kr>
2024-08-09 16:46:38 +0900

    feat : optimistic response

--- lib/user/provider/basket_provider.dart
+++ lib/user/provider/basket_provider.dart
@@ -1,20 +1,47 @@
+import 'dart:io';
+
 import 'package:actual/product/model/product_model.dart';
 import 'package:actual/restaurant/model/restaurant_detail_model.dart';
 import 'package:actual/user/model/basket_item_model.dart';
+import 'package:actual/user/model/patch_basket_body.dart';
+import 'package:actual/user/repository/user_me_repository.dart';
 import 'package:collection/collection.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 final basketProvider =
     StateNotifierProvider<BasketProvider, List<BasketItemModel>>((ref) {
-  return BasketProvider();
+  final repository = ref.watch(userMeRepositoryProvider);
+  return BasketProvider(repository: repository);
 });
 
 class BasketProvider extends StateNotifier<List<BasketItemModel>> {
-  BasketProvider() : super([]);
+  final UserMeRepository repository;
+
+  BasketProvider({required this.repository}) : super([]);
+
+  Future<void> patchBasket() async {
+    await repository.patchBasket(
+      body: PatchBasketBody(
+        basket: state
+            .map(
+              (e) => PatchBasketBodyBasket(
+                productId: e.product.id,
+                count: e.count,
+              ),
+            )
+            .toList(),
+      ),
+    );
+  }
 
   Future<void> addToBasket({
     required ProductModel product,
   }) async {
+    /*
+      요청을 먼저 보내고 응답이 오면 캐시를 업데이트 했다.
+
+     */
+
     // 1) 아직 장바구니에 해당되는 상품이 없다면, 장바구니에 상품을 추가한다.
     // 2) 만약에 이미 들어있다면 장바구니에 있는 값에 +1을 한다.
 
@@ -35,6 +62,7 @@ class BasketProvider extends StateNotifier<List<BasketItemModel>> {
         ),
       ];
     }
+    await patchBasket();
   }
 
   Future<void> removeFromBasket({
@@ -60,5 +88,6 @@ class BasketProvider extends StateNotifier<List<BasketItemModel>> {
               e.product.id == product.id ? e.copyWith(count: e.count - 1) : e)
           .toList();
     }
+    await patchBasket();
   }
 }
