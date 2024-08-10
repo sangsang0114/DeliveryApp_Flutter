1d04bad5c41f0e4c56aa5739947aa3f1334b80bc
yoon <envy15@skuniv.ac.kr>
2024-08-11 00:26:57 +0900

    fix : pagination 버그 수정

--- lib/common/model/cursor_pagination_model.dart
+++ lib/common/model/cursor_pagination_model.dart
@@ -26,11 +26,11 @@ class CursorPagination<T> extends CursorPaginationBase {
     required this.data,
   });
 
-  CursorPagination copyWith<T>({
+  CursorPagination copyWith({
     CursorPaginationMeta? meta,
     List<T>? data,
   }) {
-    return CursorPagination(
+    return CursorPagination<T>(
       meta: meta ?? this.meta,
       data: data ?? this.data,
     );
