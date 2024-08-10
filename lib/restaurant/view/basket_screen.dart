4f0310f26e18c9c954ac291562d1f45d9e026194
yoon <envy15@skuniv.ac.kr>
2024-08-10 19:04:34 +0900

    feat : 결제 기능 마무리

--- lib/restaurant/view/basket_screen.dart
+++ lib/restaurant/view/basket_screen.dart
@@ -1,9 +1,12 @@
 import 'package:actual/common/const/colors.dart';
 import 'package:actual/common/layout/default_layout.dart';
+import 'package:actual/order/provider/order_provider.dart';
+import 'package:actual/order/view/order_done_screen.dart';
 import 'package:actual/product/component/product_card.dart';
 import 'package:actual/user/provider/basket_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
 
 class BasketScreen extends ConsumerWidget {
   static String get routeName => 'basket';
@@ -106,7 +109,19 @@ class BasketScreen extends ConsumerWidget {
                       style: ElevatedButton.styleFrom(
                         backgroundColor: PRIMARY_COLOR,
                       ),
-                      onPressed: () {},
+                      onPressed: () async {
+                        final resp =
+                            await ref.read(orderProvider.notifier).postOrder();
+                        if (resp) {
+                          context.goNamed(OrderDoneScreen.routeName);
+                        } else {
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            SnackBar(
+                              content: Text('결제 실패'),
+                            ),
+                          );
+                        }
+                      },
                       child: Text(
                         '결제하기',
                         style: TextStyle(
