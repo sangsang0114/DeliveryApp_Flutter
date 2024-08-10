8f562af3c2760ff379d0ada8968f8a9843eccfa3
yoon <envy15@skuniv.ac.kr>
2024-08-10 19:52:17 +0900

    feat : throttling 적용

--- lib/common/provider/pagination_provider.dart
+++ lib/common/provider/pagination_provider.dart
@@ -2,49 +2,86 @@ import 'package:actual/common/model/cursor_pagination_model.dart';
 import 'package:actual/common/model/model_with_id.dart';
 import 'package:actual/common/model/pagination_params.dart';
 import 'package:actual/common/repository/base_pagination_repository.dart';
+import 'package:actual/restaurant/repository/restaurant_rating_repository.dart';
+import 'package:debounce_throttle/debounce_throttle.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+class _PaginationInfo {
+  final int fetchCount;
+  final bool fetchMore;
+  final bool forceRefetch;
+
+  _PaginationInfo({
+    this.fetchCount = 20,
+    this.fetchMore = false,
+    this.forceRefetch = false,
+  });
+}
+
 class PaginationProvider<T extends IModelWithId,
-        U extends IBasePaginationRepository<T>>
+U extends IBasePaginationRepository<T>>
     extends StateNotifier<CursorPaginationBase> {
   final U repository;
+  final paginationThrottle = Throttle(
+    Duration(seconds: 3),
+    initialValue: _PaginationInfo(),
+    checkEquality: false,
+  );
 
-  PaginationProvider({required this.repository})
-      : super(CursorPaginationLoading()) {
+  PaginationProvider({
+    required this.repository,
+  }) : super(CursorPaginationLoading()) {
     paginate();
+
+    paginationThrottle.values.listen(
+          (state) {
+        print(state.forceRefetch);
+        _throttledPagination(state);
+      },
+    );
   }
 
   Future<void> paginate({
     int fetchCount = 20,
-
-    //추가로 데이터 더 가져오기
-    //true : 추가로 데이터 더 가져옴
-    //false : 새로고침(현재 상태를 덮어씀)
+    // 추가로 데이터 더 가져오기
+    // true - 추가로 데이터 더 가져옴
+    // false - 새로고침 (현재 상태를 덮어씌움)
     bool fetchMore = false,
-
-    //강제로f 다시 로딩하기
-    //true : CursorPaginationLoading()
+    // 강제로 다시 로딩하기
+    // true - CursorPaginationLoading()
     bool forceRefetch = false,
   }) async {
+    print(forceRefetch);
+    paginationThrottle.setValue(_PaginationInfo(
+      fetchMore: fetchMore,
+      fetchCount: fetchCount,
+      forceRefetch: forceRefetch,
+    ));
+  }
+
+  _throttledPagination(_PaginationInfo info) async {
+    final fetchCount = info.fetchCount;
+    final fetchMore = info.fetchMore;
+    final forceRefetch = info.forceRefetch;
+
     try {
-      /*
-      5가지 가능성(State 상태)
-     */
-
+      // 5가지 가능성
+      // State의 상태
+      // [상태가]
       // 1) CursorPagination - 정상적으로 데이터가 있는 상태
-      // 2) CursorPaginationLoading - 데이터가 로딩중인 상태(현재 캐시 없음)
+      // 2) CursorPaginationLoading - 데이터가 로딩중인 상태 (현재 캐시 없음)
       // 3) CursorPaginationError - 에러가 있는 상태
-      // 4) CursorPaginationRefetching - 첫번째 페이지부터 다시 데이터를 가져올 때
-      // 5) CursorPaginationFetchMore - 추가 데이터를 paginate 해오라는 요청을 받았을 때
+      // 4) CursorPaginationRefetching - 첫번째 페이지부터 다시 데이터를 가져올때
+      // 5) CursorPaginationFetchMore - 추가 데이터를 paginate 해오라는 요청을 받았을때
 
-      //바로 반환 가능 상황
-      // 1) hasMore = false (기존 상태에서 이미 다음데이터가 없다는 값을 들고 있다면)
-      // 2) 로딩 중 - fetchMore = true
-      //    fetchMore가 아닐 때 - 새로고침의 의도가 있다(기존 요청이 중요하지 않음)
-
-      if (state is CursorPagination && forceRefetch == false) {
+      // 바로 반환하는 상황
+      // 1) hasMore = false (기존 상태에서 이미 다음 데이터가 없다는 값을 들고있다면)
+      // 2) 로딩중 - fetchMore: true
+      //    fetchMore가 아닐때 - 새로고침의 의도가 있을 수 있다.
+      if (state is CursorPagination && !forceRefetch) {
         final pState = state as CursorPagination;
-        if (!pState.meta.hasMore == false) {
+
+        if (!pState.meta.hasMore) {
           return;
         }
       }
@@ -54,9 +91,11 @@ class PaginationProvider<T extends IModelWithId,
       final isFetchingMore = state is CursorPaginationFetchingMore;
 
       // 2번 반환 상황
-      if (fetchMore && (isLoading || isRefetching || isFetchingMore)) return;
+      if (fetchMore && (isLoading || isRefetching || isFetchingMore)) {
+        return;
+      }
 
-      //paginationParams 설정
+      // PaginationParams 생성
       PaginationParams paginationParams = PaginationParams(
         count: fetchCount,
       );
@@ -66,7 +105,7 @@ class PaginationProvider<T extends IModelWithId,
       if (fetchMore) {
         final pState = state as CursorPagination<T>;
 
-        state = CursorPaginationFetchingMore<T>(
+        state = CursorPaginationFetchingMore(
           meta: pState.meta,
           data: pState.data,
         );
@@ -74,37 +113,47 @@ class PaginationProvider<T extends IModelWithId,
         paginationParams = paginationParams.copyWith(
           after: pState.data.last.id,
         );
-      } else {
-        //데이터를 처음부터 가져오는 상황
-
-        //만약에 데이터가 있는 상황이라면 기존 데이터를 보촌한채로 fetch(API 요청) 진행
-        if (state is CursorPagination && forceRefetch == false) {
+      }
+      // 데이터를 처음부터 가져오는 상황
+      else {
+        // 만약에 데이터가 있는 상황이라면
+        // 기존 데이털르 보존한채로 Fetch (API 요청)를 진행
+        if (state is CursorPagination && !forceRefetch) {
           final pState = state as CursorPagination<T>;
+
           state = CursorPaginationRefetching<T>(
             meta: pState.meta,
             data: pState.data,
           );
-        } else {
-          //나머지 상황
+        }
+        // 나미저 상황
+        else {
           state = CursorPaginationLoading();
         }
       }
 
-      final resp =
-          await repository.paginate(paginationParams: paginationParams);
+      final resp = await repository.paginate(
+        paginationParams: paginationParams,
+      );
 
       if (state is CursorPaginationFetchingMore) {
         final pState = state as CursorPaginationFetchingMore<T>;
 
-        //기존 데이터에 새로운 데이터 추가
-        state = resp.copyWith(data: [...pState.data, ...resp.data]);
+        // 기존 데이터에
+        // 새로운 데이터 추가
+        state = resp.copyWith(
+          data: [
+            ...pState.data,
+            ...resp.data,
+          ],
+        );
       } else {
         state = resp;
       }
-    } catch (e,stack) {
+    } catch (e, stack) {
       print(e);
       print(stack);
-      state = CursorPaginationError(message: '데이터를 가져오지 못했습니다');
+      state = CursorPaginationError(message: '데이터를 가져오지 못했습니다.');
     }
   }
 }
